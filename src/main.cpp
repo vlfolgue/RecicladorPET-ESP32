4298eb491e8cb7e41b1556b9c1d394a8e49e2616
victor_lof <vlfolgue@gmail.com>
2024-03-06 23:37:26 +0100

    LCD + POT + ACCEL (FALLO)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
-
+#include <Arduino.h>
 
 #include <LiquidCrystal_I2C.h>
+#include <AccelStepper.h>
+AccelStepper motor1(1, 27, 14);
+
 #define Potenciometro_velocidad 35
 
 LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
@@ -13,6 +16,9 @@ void setup()
 	lcd.backlight();
 	lcd.setCursor(3,0);
 	lcd.print("Hello, world!");
+
+		motor1.setMaxSpeed(5000); //Definimos velocidad maxima motor 1
+		motor1.setSpeed(-100);  
 }
 
 
@@ -23,6 +29,8 @@ lcd.setCursor(3,1);
 lcd.print("    ");
 lcd.setCursor(3,1);
 lcd.print(velocidad);
+
+   motor1.runSpeed();
 
 delay(100);
 }
