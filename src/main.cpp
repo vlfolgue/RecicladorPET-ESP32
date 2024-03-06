7a3d433a55883fa3f319e9c8e76fabd4681529af
victor_lof <vlfolgue@gmail.com>
2024-03-06 19:22:52 +0100

    LCD + Pontenciometro

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 
 
 #include <LiquidCrystal_I2C.h>
+#define Potenciometro_velocidad 35
 
 LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
+
+int velocidad;
 void setup()
 {
 	lcd.init(); // initialize the lcd to use user defined I2C pins
@@ -15,4 +18,11 @@ void setup()
 
 void loop()
 {
+velocidad = analogRead(Potenciometro_velocidad);
+lcd.setCursor(3,1);
+lcd.print("    ");
+lcd.setCursor(3,1);
+lcd.print(velocidad);
+
+delay(100);
 }
